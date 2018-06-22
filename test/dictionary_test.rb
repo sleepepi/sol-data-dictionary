2dc889b6c22789933518fbe988ba56684622910e
Remo Mueller <remosm@gmail.com>
2018-06-22 13:12:50 -0400

    Update files to use spout 0.14.0 configuration defaults

--- test/dictionary_test.rb
+++ test/dictionary_test.rb
@@ -1,30 +1,35 @@
-require 'test_helper'
+# frozen_string_literal: true
+
+require "test_helper"
 
 class DictionaryTest < Minitest::Test
-  # This line includes all default Spout Dictionary tests
+  # This line includes all default Spout Dictionary tests.
   include Spout::Tests
 
   # This line provides access to @variables, @forms, and @domains iterators
-  # iterators that can be used to write custom tests
+  # iterators that can be used to write custom tests.
   include Spout::Helpers::Iterators
 
-  # Example 1: Create custom tests to show that `integer` and `numeric` variables have a valid unit type
-  VALID_UNITS = ["counts per minute", "naps", "naps per weekday", "weekends", "workdays", "bouts", "lux", "naps per day", "minutes per day", "hours per day", "cups", "days", "mmol per mol", "centimeters", "", "packs", "percent", "drinks", "cigarettes",
-    "kilograms per meter squared","hours", "years", "seconds", "beats per minute", "decibels",
-    "minutes", "events per hour"] # Add your own valid units to this array
-  @variables.select{|v| v.type == 'numeric' or v.type == 'integer'}.each do |variable|
-    define_method("test_units: "+variable.path) do
-      message = "\"#{variable.units}\"".colorize( :red ) + " invalid units.\n" +
+  # Example 1: Create custom tests to show that `integer` and `numeric`
+  # variables have a valid unit type.
+  VALID_UNITS = [
+    "counts per minute", "naps", "naps per weekday", "weekends", "workdays",
+    "bouts", "lux", "naps per day", "minutes per day", "hours per day",
+    "cups", "days", "mmol per mol", "centimeters", "", "packs", "percent",
+    "drinks", "cigarettes", "kilograms per meter squared","hours", "years",
+    "seconds", "beats per minute", "decibels", "minutes", "events per hour"]
+  @variables.select { |v| %w(numeric integer).include?(v.type) }.each do |variable|
+    define_method("test_units: #{variable.path}") do
+      message = "\"#{variable.units}\"".red + " invalid units.\n" +
                 "             Valid types: " +
-                VALID_UNITS.sort.collect{|u| u.inspect.colorize( :white )}.join(', ')
+                VALID_UNITS.sort_by(&:to_s).collect { |u| u.inspect.white }.join(", ")
       assert VALID_UNITS.include?(variable.units), message
     end
   end
 
-
   # Example 2: Create custom tests to show that variables have 2 or more labels.
-  # @variables.select{|v| ['numeric','integer'].include?(v.type)}.each do |variable|
-  #   define_method("test_at_least_two_labels: "+variable.path) do
+  # @variables.select { |v| %w(numeric integer).include?(v.type) }.each do |variable|
+  #   define_method("test_at_least_two_labels: #{variable.path}") do
   #     assert_operator 2, :<=, variable.labels.size
   #   end
   # end
